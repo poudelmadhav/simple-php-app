55aa5113a09ead78b91c4a4949a7033b1ea662d1
Madhav Paudel <poudelmadhav143@gmail.com>
2018-11-21 20:15:38 +0545

    validation in category form before submitting

--- db/addform.php
+++ db/addform.php
@@ -7,16 +7,16 @@
 <body>
 	<div class="container">
 		<div class="row">
-			<div class="col-md-6 offset-md-3 mt-5">
+			<div class="col-md-6 offset-md-3 mt-5 padding-5 border p-4">
 				<h2>Add Category</h2>
-				<form method="post" action="addcategory.php">
+				<form onsubmit="return validate();" method="post" action="addcategory.php">
 					<div class="form-group">
 						<label for="name">Name</label>
 						<input type="text" name="name" id="name" class="form-control">
 					</div>
 					<div class="form-group">
 						<label for="description">Description</label>
-						<textarea id="description" name="description" class="form-control"></textarea>
+						<textarea id="description" name="description" id="description" class="form-control"></textarea>
 					</div>
 					<div>
 						<button type="submit" class="btn btn-info">Submit</button>
@@ -25,5 +25,38 @@
 			</div>
 		</div>
 	</div>
+	<script>
+		function checkValue(ele, msg) {
+			if (ele.nextSibling) {
+				ele.nextSibling.remove();
+			}
+			if (ele.value === "") {
+				ele.classList.add('is-invalid');
+				let span = document.createElement("span");
+				span.style.color = 'red';
+				span.textContent = 'This field cannot be empty. Please enter '+ msg + '.';
+				ele.parentNode.appendChild(span);
+				return false;
+			}
+			else {
+				ele.classList.remove('is-invalid');
+				return true;
+			}
+		}
+		function validate() {
+			let name = document.querySelector('#name');
+			let desc = document.querySelector('#description');
+
+			let validName = checkValue(name, 'name');
+			let validdesc = checkValue(desc, 'description');
+
+			
+			if (validName && validdesc && validEmail) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+	</script>
 </body>
 </html>
