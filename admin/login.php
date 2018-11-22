200015ba56dccbca1e70673599675736375e5cbe
Madhav Paudel <poudelmadhav143@gmail.com>
2018-11-22 23:55:32 +0545

    bucketadmin theme applied for backed login and dashboard

--- admin/login.php
+++ admin/login.php
@@ -1,39 +1,107 @@
 <?php
-	include '../config.php';
+  include '../config.php';
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title>Login - Ecommetce Admin</title>
-	<link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
+    <meta charset="utf-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="ThemeBucket">
+    <link rel="shortcut icon" href="images/favicon.png">
+
+    <title>Login - Ecommetce Admin</title>
+
+    <!--Core CSS -->
+    <link href="bs3/css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap-reset.css" rel="stylesheet">
+    <link href="font-awesome/css/font-awesome.css" rel="stylesheet" />
+
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/style-responsive.css" rel="stylesheet" />
+
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]>
+    <script src="js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
 </head>
-<body>
-	<div class="container">
-		<div class="row">
-			<div class="col-md-4 offset-4">
-				<h3>Admin Login</h3>
-				<?php if(isset($_GET['message'])) { ?>
-					<div class="alert alert-danger">
-						<?php
-							echo $_GET['message'];
-						?>
-					</div>
-				<?php } ?>
-				<form method="post" action="loginprocess.php">
-					<div class="form-group">
-						<label>Username:</label>
-						<input type="text" name="username" placeholder="Username" class="form-control" required>
-					</div>
-					<div class="form-group">
-						<label>Password:</label>
-						<input type="password" name="pwd" placeholder="Password" class="form-control" required>
-					</div>
-					<div>
-						<button type="submit" class="btn btn-success">Login</button>
-					</div>
-				</form>
-			</div>
-		</div>
-	</div>
-</body>
+
+  <body class="login-body">
+
+    <div class="container">
+
+      <form class="form-signin" action="loginprocess.php" method="post">
+        <h2 class="form-signin-heading">sign in now</h2>
+            <?php if(isset($_GET['message'])) { ?>
+              <div class="alert alert-danger text-center">
+                <?php
+                  echo $_GET['message'];
+                ?>
+              </div>
+            <?php } ?>
+        <div class="login-wrap">
+            <div class="user-login-info">
+                <input type="text" class="form-control" placeholder="Username" name="username" autofocus>
+                <input type="password" class="form-control" placeholder="Password" name="pwd">
+            </div>
+            <label class="checkbox">
+                <input type="checkbox" value="remember-me"> Remember me
+                <span class="pull-right">
+                    <a data-toggle="modal" href="#myModal"> Forgot Password?</a>
+
+                </span>
+            </label>
+            <button class="btn btn-lg btn-login btn-block" type="submit">Sign in</button>
+
+            <div class="registration">
+                Don't have an account yet?
+                <a class="" href="registration.html">
+                    Create an account
+                </a>
+            </div>
+
+        </div>
+
+          <!-- Modal -->
+          <div aria-hidden="true" aria-labelledby="myModalLabel" role="dialog" tabindex="-1" id="myModal" class="modal fade">
+              <div class="modal-dialog">
+                  <div class="modal-content">
+                      <div class="modal-header">
+                          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                          <h4 class="modal-title">Forgot Password ?</h4>
+                      </div>
+                      <div class="modal-body">
+                          <p>Enter your e-mail address below to reset your password.</p>
+                          <input type="text" name="email" placeholder="Email" autocomplete="off" class="form-control placeholder-no-fix">
+
+                      </div>
+                      <div class="modal-footer">
+                          <button data-dismiss="modal" class="btn btn-default" type="button">Cancel</button>
+                          <button class="btn btn-success" type="button">Submit</button>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <!-- modal -->
+
+      </form>
+
+    </div>
+
+
+
+    <!-- Placed js at the end of the document so the pages load faster -->
+
+    <!--Core js-->
+    <script src="js/jquery.js"></script>
+    <script src="bs3/js/bootstrap.min.js"></script>
+
+  </body>
 </html>
